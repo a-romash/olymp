6705511b371917c73fa8a021d8e52764d9a81ffa
Artem Romashov <artem20071014@gmail.com>
2023-01-23 01:35:09 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+
+start = time.time()
+# ur code =====
+
+print('Hello world')
+
+# =============
+end = time.time()
+delta = end - start
+print('Ur code was completed {0} ms.'.format(delta))
